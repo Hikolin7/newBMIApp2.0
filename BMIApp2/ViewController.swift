a0ff5988d5ea2f7c60a20e383896dd4fe04b2b55
七田　人比古 <ShichidaHitohiko@nanadenjinhiko-no-iMac.local>
2014-12-16 19:04:50 +0900

    Initial Commit

---
+++ BMIApp2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BMIApp2
+//
+//  Created by 七田　人比古 on 2014/12/16.
+//  Copyright (c) 2014年 Shichida Hitohiko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ BMIApp2Tests/BMIApp2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  BMIApp2Tests.swift
+//  BMIApp2Tests
+//
+//  Created by 七田　人比古 on 2014/12/16.
+//  Copyright (c) 2014年 Shichida Hitohiko. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class BMIApp2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
